c0fb808a2d37c302bc1b4aa8e36eef27005d5b68
Sushma <nellisushma@gmail.com>
2023-10-24 16:41:08 -0500

    Terraform

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -19,3 +19,58 @@ output "sample1" {
 output "sample-ext" {
   value = "Value of Sample & Sample1 - ${var.sample} - ${var.sample1}"
 }
+
+# Variable Data types
+# 1. string
+# 2. number
+# 3. Boolean
+
+# Variable Types
+# In Ansible - 1. Plain Key, Value, 2. List 3. Map/Dict
+
+# In Terraform
+# 1. Plain
+# 2. List
+# 3. Map
+
+## Plain
+variable "course" {
+  default = "DevOps Training"
+}
+
+# List
+variable "courses" {
+  default = [
+    "DevOps",
+    "AWS",
+    "Python"
+  ]
+}
+
+## Map
+variable "course_details" {
+  default = {
+    DevOps = {
+      name     = "DevOps"
+      timing   = "10am"
+      duration = 90
+    }
+    AWS = {
+      name     = "AWS"
+      timing   = "11am"
+      duration = 30
+    }
+  }
+}
+
+output "course" {
+  value = var.course
+}
+
+output "courses" {
+  value = var.courses
+}
+
+output "course_details" {
+  value = var.course_details
+}
