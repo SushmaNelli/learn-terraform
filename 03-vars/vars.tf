05ac6c1b654b8449e95fec96b78fbf2e5f5804f9
Sushma <nellisushma@gmail.com>
2023-10-24 16:46:29 -0500

    Terraform

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -68,9 +68,9 @@ output "course" {
 }
 
 output "courses" {
-  value = var.courses
+  value = var.courses[2]
 }
 
 output "course_details" {
-  value = var.course_details
+  value = var.course_details["Devops"]
 }
