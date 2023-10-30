72f1b9694e9b5e5520bc9554b9bc68a485d19473
Sushma <nellisushma@gmail.com>
2023-10-30 15:27:26 -0500

    Terraform

--- 08-provisioner/main.tf
+++ 08-provisioner/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
-vpc_security_group_ids = {"sg-0710514ddbbda24c3"}
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0710514ddbbda24c3"]
   tags = {
     Name = "HelloWorld"
   }
