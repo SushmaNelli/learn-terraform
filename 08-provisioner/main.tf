ea74e404ec0c307e9849a8cc4d4dda01e7f1279c
Sushma <nellisushma@gmail.com>
2023-10-30 15:22:46 -0500

    Terraform

---
+++ 08-provisioner/main.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+vpc_security_group_ids = {"sg-0710514ddbbda24c3"}
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -u https://github.com/SushmaNelli/roboshop-ansible main.yml -e env=dev -e role_name=frontend",
+    ]
+  }
+
+}
+
+data "aws_ami" "example" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
